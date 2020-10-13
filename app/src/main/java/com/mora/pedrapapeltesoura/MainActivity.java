9cf10d8d12e47e3c31f6f6c5d78f469b302629f5
adriano <adrianoskte@gmail.com>
2020-10-13 15:11:38 -0300

    project part 2

--- app/src/main/java/com/mora/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/mora/pedrapapeltesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.mora.pedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    public  void SelecionadoPedra(View view){
+
+
+        this.opcaoSelecionada("Pedra");
+
+    }
+    public  void SelecionadoPapel(View view){
+
+        this.opcaoSelecionada("Papel");
+
+    }
+    public  void SelecionadoTesoura(View view){
+
+        this.opcaoSelecionada("Tesoura");
+
+    }
+
+     public  void opcaoSelecionada(String opcaoSelecionada){
+
+        System.out.println("O item clicado foi " + opcaoSelecionada);
+
+     }
 }
